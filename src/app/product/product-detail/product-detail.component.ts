f803d411ae6804c441e7e2ed76a099fd7996ccd5
Nakai Yuto <ny,bin1210@gmail.com>
2021-01-26 22:28:42 +0900

    commit

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../../products';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+
+export class ProductDetailComponent implements OnInit {
+  product: string | undefined;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
+    this.route.paramMap.subscribe(
+      params => {
+        if (params != null) {
 
+          // this.product = this.products[+params.get('productId')]
+        }
+      })
+  }
 }
