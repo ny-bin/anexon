1cd75a8cccaf1f218792a53717c4b674010eebc3
Nakai Yuto <ny,bin1210@gmail.com>
2021-01-27 23:11:22 +0900

    perf create front

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,17 +1,38 @@
 export const products = [
-    {
-      name: 'Phone XL',
-      price: 799,
-      description: 'A large phone with one of the best screens'
-    },
-    {
-      name: 'Phone Mini',
-      price: 699,
-      description: 'A great phone with one of the best cameras'
-    },
-    {
-      name: 'Phone Standard',
-      price: 299,
-      description: 'test'
-    }
-  ];
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'sample text',
+    heading2: 'サンプル',
+    heading3: 'sample text',
+    headingText1: 'heading text sample1',
+    headingText2: 'heading text sample3',
+    headingText3: 'heading text sample2',
+    coverImage: './assets/img/phone-cover.jpg',
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'sample text',
+    heading2: 'サンプル',
+    heading3: 'sample text',
+    headingText1: 'heading text sample1',
+    headingText2: 'heading text sample3',
+    headingText3: 'heading text sample2',
+    coverImage: 'data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==',
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: 'test',
+    heading1: 'sample text',
+    heading2: 'サンプル',
+    heading3: 'sample text',
+    headingText1: 'heading text sample1',
+    headingText2: 'heading text sample3',
+    headingText3: 'heading text sample2',
+    coverImage: 'data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==',
+  }
+];
