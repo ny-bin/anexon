b0562a5a62e9f6ee6fd04f046da3f7c3b3733386
Nakai Yuto <ny,bin1210@gmail.com>
2021-01-30 20:57:43 +0900

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,10 @@ import { NavbarComponent } from './common/navbar/navbar.component';
     AppComponent,
     NavbarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule,],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,],
   providers: [],
   bootstrap: [AppComponent],
 })
